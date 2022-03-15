9c56abb643b3a31153475c897400025f76f28101
BuildTools <unconfigured@null.spigotmc.org>
2022-03-15 20:01:40 +0400

    Material

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,29 +7,37 @@ const Home: NextPage = () => {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Saeko</title>
-        <meta name="description" content="Saeko&apos;s website" />
+        <title>hmmmm</title>
+        <meta name="description" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
+      <main className={styles.description}>
         <h1 className={styles.title}>
           Hello!
         </h1>
 
         <p className={styles.description}>
-          idk what to put here and im too quirky like dat to remove it
+          This is where I put all my stuff
         </p>
 
-        <div className={styles.grid}>
+        <p className={styles.description}>
+          ==================================================
+        </p>
+
+        <div className={styles.header}>
           <a href="https://drive.google.com/drive/folders/11XyJfKZqPPUBeUSG_SyZkasIttUGujsR?usp=sharing">
-            <h2>SVP files &rarr;</h2>
+            <h2>SVP files</h2>
             <p>SVP files I probably just got by 
               editing someone&apos;s VSQX or UST</p>
           </a>
 
+          <p className={styles.header}>
+          --------------------------------------------------------------------------------
+        </p>
+
           <a href="https://discord.gg/rsVMyBqP">
-            <h2>Discord Server &rarr;</h2>
+            <h2>Discord Server</h2>
             <p>ded</p>
           </a>
 
@@ -38,7 +46,7 @@ const Home: NextPage = () => {
         </main>
 
         <footer className={styles.footer}>
-            Website by Saeko
+            made with the loss of my nerd points
         </footer>
       </div>
   )
